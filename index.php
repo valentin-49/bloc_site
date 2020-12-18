fff3273fede9b94e0f98f8a1a0d32b5872dcb1ec
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2020-12-18 15:57:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,14 +4,30 @@
         <link rel="stylesheet" type="text/css" href="fichier_bootstrap/bootstrap-4.5.3-dist/css/bootstrap.css">
     </head>
     <body class="background">
+            <div class="header_main">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6" id="title" align="center"> 
+                        Taxi Bouet
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-2 col-xl-2" align="center">
+                        <button type="submit" class="submit-btn">Acceuils</button>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-2 col-xl-2" align="center">
+                        <button type="submit" class="submit-btn">Nos services</button>  
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-2 col-xl-2" align="center">
+                        <button type="submit" class="submit-btn">Nous contacter</button>   
+                    </div>
+                </div>  
+            </div>  
         <div class="page">
             <div class="bloc">
                 <div class="row content">
                     <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <img src="img/Desert.jpg" class="responsive" >
+                        <img src="img/taxi_bouet.jpg" class="responsive" >
                     </div>
                     <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <p class="text">
+                        <p id="text">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -21,41 +37,7 @@
                         </p>
                     </div>
                 </div>
-            </div>
-            <div class="bloc">
-                <div class="row content">
-                    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <img src="img/Desert.jpg" class="responsive" >
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <p class="text">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="bloc">
-                <div class="row content">
-                    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <img src="img/Desert.jpg" class="responsive" >
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <p class="text">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            </div>    
         </div>
     </body>
 
