48f68ad271eb6ced412e400bf2e43b77f69b372f
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2020-12-18 16:22:35 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                         Taxi Bouet
                     </div>
                     <div class="col-sm-12 col-md-12 col-lg-2 col-xl-2" align="center">
-                        <button type="submit" class="submit-btn">Acceuils</button>
+                        <button type="submit" class="submit-btn">Acceuil</button>
                     </div>
                     <div class="col-sm-12 col-md-12 col-lg-2 col-xl-2" align="center">
                         <button type="submit" class="submit-btn">Nos services</button>  
@@ -37,8 +37,24 @@
                         </p>
                     </div>
                 </div>
-            </div>    
+            </div>
+            <div class="bloc">
+                <div class="row content">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
+                        <p id="text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
+                        <img src="img/taxi_bouet.jpg" class="responsive" >
+                    </div>
+                </div>
+            </div>        
         </div>
     </body>
-
 </html>
