7a559defa9b9256a9134f604039c64a29d8f6ab6
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2020-12-18 16:32:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <html>
     <head>
+    <meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+
+<!-- Make sure you put this AFTER Leaflet's CSS -->
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
         <link rel="stylesheet" type="text/css" href="page.css">
         <link rel="stylesheet" type="text/css" href="fichier_bootstrap/bootstrap-4.5.3-dist/css/bootstrap.css">
     </head>
@@ -51,7 +58,35 @@
                         </p>
                     </div>
                     <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 " align="center">
-                        <img src="img/taxi_bouet.jpg" class="responsive" >
+                        <div id="mapid" style="width: 800px; height: 500px;float: center;"></div>
+                            <script>
+                                var mymap = L.map('mapid').setView([47.05, -0.87], 10);
+
+                                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                                    maxZoom: 18,
+                                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                                        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                                    id: 'mapbox/streets-v11',
+                                    tileSize: 512,
+                                    zoomOffset: -1
+                                }).addTo(mymap);
+
+                                L.marker([8.86578, -79.4971]).addTo(mymap);
+
+                                L.circle([8.86578, -79.4971], {
+                                    color: 'red',
+                                    fillColor: '#f03',
+                                    fillOpacity: 0.5,
+                                    radius: 500
+                                }).addTo(mymap);
+
+                                L.polygon([
+                                    [51.509, -0.08],
+                                    [51.503, -0.06],
+                                    [51.51, -0.047]
+                                ]).addTo(mymap);
+                            </script>
                     </div>
                 </div>
             </div>        
